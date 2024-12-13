3b70f43bc67d7d962bf9f1f393221684a777166a
Joan Gerard <joan.gerard@outlook.com>
2024-12-13 18:24:29 +0100

    create Hero

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,34 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 import React from "react";
 
 export default function Home() {
   return (
     <div className="">
-      <div className="h-screen">
-        <Image
+      <div
+        className="h-screen"
+        style={{
+          backgroundImage: `url('/home-hero-2.jpg')`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "100vh",
+          color: "white",
+          margin: 0,
+          padding: 0,
+          display: "flex",
+          flexDirection: "column" as const,
+          alignItems: "center",
+        }}
+      >
+        {/* <Image
           src="/home-hero-2.jpg"
           alt=""
           fill
           style={{
             objectFit: "cover",
           }}
-        />
-      </div>
-      <div className="">
-        <p>Section 2</p>
+        /> */}
+        <Hero />
       </div>
     </div>
   );
---
+++ components/Hero.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="h-screen text-white w-full flex items-center">
+      <div className="pl-6 md:pl-24 flex flex-col gap-y-6">
+        <h2 className="text-3xl md:text-6xl flex flex-col font-black uppercase gap-y-1 md:gap-y-4 font-work-sans">
+          <span>Development +</span>
+          <span>Management</span>
+        </h2>
+        <div className="text-balance md:w-[60%] 2xl:w-[50%] text-base md:text-lg">
+          <p>
+            Joan Gerard is a Software Developer and Project Lead. He is
+            passionate about creating innovative digital solutions, driving team
+            collaboration, and continuously improving processes to deliver
+            high-quality, user-centric applications.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
