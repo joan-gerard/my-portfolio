cdbffd62146d07fec83ae48089995c4b8742a956
Joan Gerard <joan.gerard@outlook.com>
2025-02-04 18:54:06 +0100

    fix layout issue

--- app/(root)/work/[_slug]/page.tsx
+++ app/(root)/work/[_slug]/page.tsx
@@ -1,6 +1,5 @@
 import { CodeCard } from "@/components/CodeCard";
 import { StickyImageNew } from "@/components/parallax/StickyImageNew";
-import Reveal from "@/components/utils/Reveal";
 import { SectionHeader } from "@/components/utils/SectionHeader";
 import { work } from "@/constants/work";
 
@@ -9,48 +8,42 @@ const page = async ({ params }: { params: Promise<{ _slug: string }> }) => {
   const project = work.find((el) => el.slug === slug);
 
   return (
-    <Reveal>
-      <section className="bg-black h-full z-0 relative">
-        {/* <Hero /> */}
-        <SectionHeader
-          title={project!.subheading}
-          dir="l"
-          className="mb-10 mt-32 px-6 my-8 lg:px-24 xl:px-36 mx-auto"
-        />
+    <div className="bg-black h-full z-0 relative">
+      {/* <Hero /> */}
+      <SectionHeader title={project!.subheading} dir="l" className="mb-10 mt-32 px-6 my-8 lg:px-24 xl:px-36 mx-auto" />
 
-        <StickyImageNew imgUrl={project!.imgUrl} />
-        <div className="mx-auto max-w-5xl gap-8 flex px-4 pb-24 pt-12 text-white z-0">
-          <p>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Id bibendum dis
-            imperdiet pulvinar blandit pulvinar; sollicitudin justo. Sit
-            pellentesque potenti volutpat vulputate metus. Suspendisse neque
-            porttitor sed sollicitudin elit magna. Posuere aenean parturient
-            ullamcorper ad mus parturient. Consectetur donec congue suscipit
-            laoreet taciti natoque class arcu. Litora efficitur est potenti;
-            penatibus viverra mus bibendum rutrum libero. Suscipit dui facilisis
-            senectus iaculis in suscipit mi leo.
-          </p>
-          <p>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Id bibendum dis
-            imperdiet pulvinar blandit pulvinar; sollicitudin justo. Sit
-            pellentesque potenti volutpat vulputate metus. Suspendisse neque
-            porttitor sed sollicitudin elit magna. Posuere aenean parturient
-            ullamcorper ad mus parturient. Consectetur donec congue suscipit
-            laoreet taciti natoque class arcu. Litora efficitur est potenti;
-            penatibus viverra mus bibendum rutrum libero. Suscipit dui facilisis
-            senectus iaculis in suscipit mi leo.
-          </p>
-        </div>
-        <div className="pb-24">
-          <CodeCard
-            githubUrl={project?.githubUrl}
-            liveUrl={project?.liveUrl}
-            javascriptCode={project?.javascriptCode}
-            pythonCode={project?.pythonCode}
-          />
-        </div>
-      </section>
-    </Reveal>
+      <StickyImageNew imgUrl={project!.imgUrl} />
+      <div className="mx-auto max-w-5xl gap-8 flex px-4 pb-24 pt-12 text-white z-0">
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Id bibendum dis
+          imperdiet pulvinar blandit pulvinar; sollicitudin justo. Sit
+          pellentesque potenti volutpat vulputate metus. Suspendisse neque
+          porttitor sed sollicitudin elit magna. Posuere aenean parturient
+          ullamcorper ad mus parturient. Consectetur donec congue suscipit
+          laoreet taciti natoque class arcu. Litora efficitur est potenti;
+          penatibus viverra mus bibendum rutrum libero. Suscipit dui facilisis
+          senectus iaculis in suscipit mi leo.
+        </p>
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Id bibendum dis
+          imperdiet pulvinar blandit pulvinar; sollicitudin justo. Sit
+          pellentesque potenti volutpat vulputate metus. Suspendisse neque
+          porttitor sed sollicitudin elit magna. Posuere aenean parturient
+          ullamcorper ad mus parturient. Consectetur donec congue suscipit
+          laoreet taciti natoque class arcu. Litora efficitur est potenti;
+          penatibus viverra mus bibendum rutrum libero. Suscipit dui facilisis
+          senectus iaculis in suscipit mi leo.
+        </p>
+      </div>
+      <div className="pb-24">
+        <CodeCard
+          githubUrl={project?.githubUrl}
+          liveUrl={project?.liveUrl}
+          javascriptCode={project?.javascriptCode}
+          pythonCode={project?.pythonCode}
+        />
+      </div>
+    </div>
   );
 };
 
