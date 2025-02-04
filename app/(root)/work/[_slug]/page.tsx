a7c5b33b221ec6810c9b968048ce7200da7d37b2
Joan Gerard <joan.gerard@outlook.com>
2025-02-04 19:07:03 +0100

    fix layout issue

--- app/(root)/work/[_slug]/page.tsx
+++ app/(root)/work/[_slug]/page.tsx
@@ -1,5 +1,6 @@
 import { CodeCard } from "@/components/CodeCard";
 import { StickyImageNew } from "@/components/parallax/StickyImageNew";
+import Reveal from "@/components/utils/Reveal";
 import { SectionHeader } from "@/components/utils/SectionHeader";
 import { work } from "@/constants/work";
 
@@ -8,42 +9,48 @@ const page = async ({ params }: { params: Promise<{ _slug: string }> }) => {
   const project = work.find((el) => el.slug === slug);
 
   return (
-    <div className="bg-black h-full z-0 relative">
-      {/* <Hero /> */}
-      <SectionHeader title={project!.subheading} dir="l" className="mb-10 mt-32 px-6 my-8 lg:px-24 xl:px-36 mx-auto" />
-
-      <StickyImageNew imgUrl={project!.imgUrl} />
-      <div className="mx-auto max-w-5xl gap-8 flex px-4 pb-24 pt-12 text-white z-0">
-        <p>
-          Lorem ipsum odor amet, consectetuer adipiscing elit. Id bibendum dis
-          imperdiet pulvinar blandit pulvinar; sollicitudin justo. Sit
-          pellentesque potenti volutpat vulputate metus. Suspendisse neque
-          porttitor sed sollicitudin elit magna. Posuere aenean parturient
-          ullamcorper ad mus parturient. Consectetur donec congue suscipit
-          laoreet taciti natoque class arcu. Litora efficitur est potenti;
-          penatibus viverra mus bibendum rutrum libero. Suscipit dui facilisis
-          senectus iaculis in suscipit mi leo.
-        </p>
-        <p>
-          Lorem ipsum odor amet, consectetuer adipiscing elit. Id bibendum dis
-          imperdiet pulvinar blandit pulvinar; sollicitudin justo. Sit
-          pellentesque potenti volutpat vulputate metus. Suspendisse neque
-          porttitor sed sollicitudin elit magna. Posuere aenean parturient
-          ullamcorper ad mus parturient. Consectetur donec congue suscipit
-          laoreet taciti natoque class arcu. Litora efficitur est potenti;
-          penatibus viverra mus bibendum rutrum libero. Suscipit dui facilisis
-          senectus iaculis in suscipit mi leo.
-        </p>
-      </div>
-      <div className="pb-24">
-        <CodeCard
-          githubUrl={project?.githubUrl}
-          liveUrl={project?.liveUrl}
-          javascriptCode={project?.javascriptCode}
-          pythonCode={project?.pythonCode}
+    <Reveal>
+      <div className="px-6 my-8 lg:px-24 xl:px-36 mx-auto">
+        {/* <Hero /> */}
+        <SectionHeader
+          title={project!.subheading}
+          dir="l"
+          className="mb-10 mt-32 "
         />
+
+        <StickyImageNew imgUrl={project!.imgUrl} />
+        <div className="mx-auto gap-8 flex px-4 pb-24 pt-12 text-white z-0">
+          <p>
+            Lorem ipsum odor amet, consectetuer adipiscing elit. Id bibendum dis
+            imperdiet pulvinar blandit pulvinar; sollicitudin justo. Sit
+            pellentesque potenti volutpat vulputate metus. Suspendisse neque
+            porttitor sed sollicitudin elit magna. Posuere aenean parturient
+            ullamcorper ad mus parturient. Consectetur donec congue suscipit
+            laoreet taciti natoque class arcu. Litora efficitur est potenti;
+            penatibus viverra mus bibendum rutrum libero. Suscipit dui facilisis
+            senectus iaculis in suscipit mi leo.
+          </p>
+          <p>
+            Lorem ipsum odor amet, consectetuer adipiscing elit. Id bibendum dis
+            imperdiet pulvinar blandit pulvinar; sollicitudin justo. Sit
+            pellentesque potenti volutpat vulputate metus. Suspendisse neque
+            porttitor sed sollicitudin elit magna. Posuere aenean parturient
+            ullamcorper ad mus parturient. Consectetur donec congue suscipit
+            laoreet taciti natoque class arcu. Litora efficitur est potenti;
+            penatibus viverra mus bibendum rutrum libero. Suscipit dui facilisis
+            senectus iaculis in suscipit mi leo.
+          </p>
+        </div>
+        <div className="pb-24">
+          <CodeCard
+            githubUrl={project?.githubUrl}
+            liveUrl={project?.liveUrl}
+            javascriptCode={project?.javascriptCode}
+            pythonCode={project?.pythonCode}
+          />
+        </div>
       </div>
-    </div>
+    </Reveal>
   );
 };
 
--- constants/work.ts
+++ constants/work.ts
@@ -2,8 +2,8 @@ export const work = [
   {
     imgUrl:
       "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    subheading: "Porfolio",
-    slug: "porfolio",
+    subheading: "Portfolio",
+    slug: "portfolio",
     heading: "Built for all of us.",
     additionalTitle: "Additional content explaining the above card here",
     description: [
@@ -94,8 +94,8 @@ app.do_cool_thing()`,
   {
     imgUrl:
       "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    subheading: "Porfolio 2",
-    slug: "porfolio-2",
+    subheading: "Portfolio 2",
+    slug: "portfolio-2",
     heading: "Built for all of us.",
     additionalTitle: "Additional content explaining the above card here",
     description: [
