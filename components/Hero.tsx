e76578d0a363616a07cf10aabc0c29350f2373cd
Joan Gerard <joan.gerard@outlook.com>
2025-02-05 14:35:17 +0100

    update Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,13 +4,14 @@ const Hero = () => {
   return (
     <div className="h-screen text-white w-full flex items-center">
       <div className="pl-6 md:pl-24 flex flex-col gap-y-6">
-        <h2 className="text-3xl md:text-6xl flex flex-col font-black uppercase gap-y-1 md:gap-y-4 font-work-sans">
-          <span>Development +</span>
-          <span>Management</span>
+        <h2 className="text-3xl md:text-5xl flex flex-col font-black uppercase gap-y-1 md:gap-y-4 font-work-sans lg:w-1/2 text-balance">
+          Software Development & Management
         </h2>
-        <div className="text-balance md:w-[60%] 2xl:w-[50%] text-base md:text-lg flex flex-col gap-y-2">
-          <p>Joan Gerard is a Software Developer and Project Lead.</p>
-          <p>
+        <div className="text-balance 2xl:w-[50%] flex flex-col gap-y-3">
+          <p className="text-lg md:text-xl">
+            Joan Gerard leads and builds software projects.
+          </p>
+          <p className="text-base md:text-lg">
             He is passionate about creating innovative digital solutions,
             driving team collaboration, and continuously improving processes to
             deliver high-quality, user-centric applications.
